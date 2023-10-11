1db6a5d3b88b3f95c26a541c5b445f68aa58b75f
WilliamPeSouza <williampesouza@gmail.com>
2023-10-11 15:59:24 -0300

    atualizado

--- mosaicov2/js/video.js
+++ mosaicov2/js/video.js
@@ -1,13 +1,34 @@
 const html = document.querySelector('html')
 const player = document.getElementById('player')
 const videos = document.querySelectorAll('.videos')
+let video = document.getElementById('video')
+const arrSrc = ['./galeria/videos/01_fiat.mp4', 
+              './galeria/videos/02_deezer.mp4',
+              './galeria/videos/03_havaianas.mp4', 
+              './galeria/videos/04_veja.mp4', 
+              './galeria/videos/05_meta.mp4', 
+              './galeria/videos/06_samsung.mp4', 
+              './galeria/videos/07_loggi.mp4', 
+              './galeria/videos/08_outback.mp4', 
+              './galeria/videos/09_only.mp4', 
+              './galeria/videos/10_seara.mp4', 
+              './galeria/videos/11_big.mp4', 
+              './galeria/videos/12_elseve.mp4', 
+              './galeria/videos/13_lopes.mp4', 
+              './galeria/videos/14_vanish.mp4',
+              './galeria/videos/15_timburtn.mp4', 
+              './galeria/videos/16_livelo.mp4', 
+              './galeria/videos/17_benegrip.mp4', 
+              './galeria/videos/18_globoplay.mp4']
 
 
-videos.forEach(i => {
+videos.forEach((i, index) => {
   i.addEventListener('click', function() {
     removeClass(player, 'video-off')
     addClass(player, 'video-on')
     html.style.overflowY = 'hidden'
+    video.src = arrSrc[index]
+    video.play();
   })
 })
 
@@ -17,6 +38,7 @@ player.addEventListener('click', function (e) {
     addClass(player, 'video-off')
     removeClass(player, 'video-on')
     html.style.overflowY = 'scroll'
+    video.pause();
   }
 })
 
@@ -26,3 +48,9 @@ function addClass(element, className) {
 function removeClass(element, className) {
   element.classList.remove(className)
 }
+
+
+console.log(videos.length)
+
+
+
